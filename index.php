b36e69b0b8325757e4691b5b49a8e7c285f2ac6f
lucasjmoore <lucasmoore88@gmail.com>
2015-02-22 13:45:30 -0500

    added needed font awesome backs locally

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
      <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ReportCar</title>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome-4.3.0/css/font-awesome.css">
     <link rel="stylesheet" type="text/css" href="css/flatly.css">
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
